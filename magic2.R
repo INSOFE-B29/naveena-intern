84fb42489550cf46bb439ebd6fce881230078ae5
naveena1994 <kamarthapu.navi@gmail.com>
2017-09-29 19:43:57 +0530

    Add files via upload

--- magic2.R
+++ magic2.R
@@ -9,8 +9,7 @@ sum(is.na(magicdata))
 
 str(magicdata)
 library(caret)
-
-    
+#unique(magicdata$class)
 magicdata$class<-ifelse(magicdata$class=="g",0,1)
 table(magicdata$class)
 magicdata$class<-as.factor(magicdata$class)
@@ -50,9 +49,16 @@ plot(perf, col=rainbow(10), colorize=T)
 perf_auc <- performance(pred, measure="auc")
 auc <- perf_auc@y.values[[1]]
 print(auc)
-pred_test.svm <- predict(model_svm,test.data,type = "response")
+pred_test.svm <- predict(model_svm,test.data)
+confusionMatrix(test.data$class,pred_test.svm)$overall[1]
 confusionMatrix(test.data$class,pred_test.svm)
+preds_train_svm<-predict(model_svm)
 
+#model build w.o pca
+svm_wopca<-svm(class~.,train.data.md,kernel="linear",type='C-classification',cost=20)
+pred<-predict(svm_wopca,test.data.md)
+confusionMatrix(pred,test.data.md$class)
+preds_train_svmwopca<-predict(svm_wopca)
 #model_svm <- tune.svm(class~., data = train.data,kernel = "linear",cost=2^(1:2)) 
 #preds.model=predict(model_svm,test.data,type="class")
 #conf_matrix <- table(test.data$class, preds.model)
@@ -69,6 +75,7 @@ model_aic <- stepAIC(log_reg, direction = "both")
 summary(model_aic)
 summary(log_reg)
 library(car)
+
 vif(model_aic)
 vif(log_reg)
 # roc on train
@@ -83,46 +90,101 @@ auc <- perf_auc@y.values[[1]]
 print(auc)
 #Choose a Cutoff Value
 #Predictions on test data
-prob_test <- predict(log_reg, test.data, type = "response")
+prob_test <- predict(log_reg, test.data,type = "response")
 preds_test <- ifelse(prob_test > 0.65, "1", "0")
-#preds.train<-ifelse(prob_train>0.65,1,0)
 confusionMatrix(test.data$class,preds_test)
+#prediction on train 
+preds_train_log<-predict(log_reg,train.data,type = "response")
+table(preds_train_log)
+preds_train_lpgprob <- ifelse(preds_train_log >0.65, "1", "0")
+confusionMatrix(preds_train_lpgprob,train.data$class)
+
+
+#logestic wo pca
+log_reg.wopca <- glm(class~., data = train.data.md, family = binomial)
+model_aic.wopca <- stepAIC(log_reg.wopca, direction = "both")
+summary(model_aic.wopca)
+summary(log_reg.wopca)
+library(car)
+vif(model_aic.wopca)
+vif(log_reg.wopca)
+prob_train.wopca <- predict(log_reg.wopca, type = "response")
+library(ROCR)
+pred.wopca <- prediction(prob_train.wopca, train.data.md$class)
+perf.wopca <- performance(pred.wopca, measure="tnr", x.measure="fnr")
+plot(perf.wopca, col=rainbow(10), colorize=T, print.cutoffs.at=seq(0,1,0.05))
+#auc
+perf_auc.wopca <- performance(pred.wopca, measure="auc")
+auc.wopca <- perf_auc.wopca@y.values[[1]]
+print(auc.wopca)
+#Choose a Cutoff Value
+#Predictions on test data
+prob_test.wopca <- predict(log_reg.wopca, test.data.md, type = "response")
+preds_test.wopca <- ifelse(prob_test.wopca > 0.7, "1", "0")
+#preds.train<-ifelse(prob_train>0.65,1,0)
+confusionMatrix(test.data.md$class,preds_test.wopca)
+
+#let's now remove the "fconc" variable, as it is the lesser significant of the two
+#magicdatafcon<-magicdata
+#str(magicdatafcon)
+#str(magicdata)
+#cor(magicdata$fConc,magicdata$fConc1)
+#modellgcor<- glm(class~., data = train.data.md[,-4], family = binomial)
+#str(modellgcor)
+#pred.fcon<-predict(modellgcor,test.data.md)
+#test.data.md$fConc<-NULL
+#str(test.data.md)
+#confusionMatrix(pred.fcon,test.data.md[,-4]$class)
 
 
 #SVM kernel="anovadot"
 library(kernlab)
-model_svm <- ksvm(class ~ . , train.data,kernel="anovadot")
-pred<-predict(model_svm,test.data)
+model_svm.ano <- ksvm(class ~ . , train.data,kernel="anovadot")
+pred.ano<-predict(model_svm.ano,test.data)
 #tanhdot(scale = 1, offset = 1)
+confusionMatrix(pred.ano,test.data$class)$overall[1]
+confusionMatrix(pred.ano,test.data$class)
+preds_train_ano<-predict(model_svm.ano)
+
+#SVM kernel="anovadot" w.o pca
+model.svm.wopca <- ksvm(class ~ . , train.data.md,kernel="anovadot",sigma = 3, degree = 4)
+pred<-predict(model.svm.wopca,test.data.md)
 confusionMatrix(pred,test.data$class)
+preds_train_svmwopca<-predict(model.svm.wopca)
+
 
 #SVM kernel="tanhdot"
 #train svm using custom kernel
-model<-ksvm(class~.,data=train.data,kernel="tanhdot", C=5,cross=5)
-pred<-predict(model_svm,test.data)
+model.tanhdot<-ksvm(class~.,data=train.data,kernel="tanhdot", C=5,cross=5)
+pred.tan<-predict(model.tanhdot,test.data)
 str(pred)
 table(pred)
 #tanhdot(scale = 1, offset = 1)
-confusionMatrix(pred,test.data$class)
-
+confusionMatrix(pred.tan,test.data$class)$overall[1]
+confusionMatrix(pred.tan,test.data$class)
+preds_train_tanhdot<-predict(model.tanhdot)
 #prioir_svm <- tune.svm(formul=class~.,data=train.data, cost =0.1 , gamma =0.4,tunecontrol = tc)
 
 #decision tree CART
 #tc <- tune.control(cross = 5)
 library(rpart)
 #Model the tree
-dc_tree.model <- rpart(class~.,train.data,control = rpart.control(cp =0.0005))
+dc_tree.model <- rpart(class~.,train.data,control = rpart.control(cp =0.01))
+dc_fit <- train(class~., data = train.data, method = "rpart", trControl=trctrl,preProcess = c("center", "scale"),tuneLength = 10)
 summary(dc_tree.model)
 #Tree Explicability
 #The variable importance can accessed accessing variable.importance from the tree list
 dc_tree.model$variable.importance
 library(rpart.plot)
 rpart.plot(dc_tree.model)
-pred.dc_model <- predict(dc_tree.model, test.data)
+pred.dc_model <- predict(dc_fit, test.data)
 #control = rpart.control(minsplit = 20, xval = 81, cp = 0.01)
 str(pred.dc_model)
-preds_tree <- ifelse(pred.dc_model[, 1] > pred.dc_model[, 2], 0, 1)
-confusionMatrix(preds_tree,test.data$class)
+#preds_tree <- ifelse(pred.dc_model[, 1] > pred.dc_model[, 2], 0, 1)
+confusionMatrix(pred.dc_model,test.data$class)$overall[1]
+confusionMatrix(pred.dc_model,test.data$class)
+preds_train_dc<-predict(dc_tree.model)
+preds_train_tree <- ifelse(preds_train_dc[, 1] > preds_train_dc[, 2], 0, 1)
 
 #finding cp value
 dtCart=rpart(class ~.,data=train.data,method="class")
@@ -131,23 +193,26 @@ cp[cp$xstd==min(cp$xstd),][1,]$CP
 
 #c5.0 moel building
 library(C50)
-
 c5_tree <- C5.0(class ~ . , train.data,cp=1,ntrees=1500)
 c5_rules <- C5.0(class ~ . , train.data,rules=T)
 #Variable Importance in trees
 C5imp(c5_tree, metric = "usage")
 summary(c5_rules)
 plot(c5_tree)
-preds <- predict(c5_tree, test.data)
+preds.c5 <- predict(c5_tree, test.data)
 library(caret)
-confusionMatrix(preds, test.data$class)
+confusionMatrix(preds.c5, test.data$class)$overall[1]
+confusionMatrix(preds.c5, test.data$class)
+
+
 
 
 #Bagged Decision Trees
 library(ipred)
 set.seed(1234)
-
 model_tree_bag <- bagging(class ~ . , data=train.data, control = rpart.control(cp=.0001))
+
+
 preds_tree_bag <- predict(model_tree_bag, test.data)
 confusionMatrix(preds_tree_bag, test.data$class)
 #dtCart=rpart(class ~.,data=train.data,method="class")
@@ -157,6 +222,7 @@ confusionMatrix(preds_tree_bag, test.data$class)
 library(caret)
 preds_tree_bag <- predict(model_tree_bag, train.data)
 confusionMatrix(preds_tree_bag, train.data$class)
+preds_train_bdt<-predict(model_tree_bag)
 
 #xgboost
 library(vegan)
@@ -178,6 +244,7 @@ caret::confusionMatrix(train.data$class,ifelse(y_pred >= 1.5,1,0))
 y_pred = predict(xgmodel, newdata = as.matrix(test.data[-8]))
 table (test.data$class,ifelse(y_pred >= 1.5,1,0))
 caret::confusionMatrix(test.data$class,ifelse(y_pred >= 1.5,1,0))
+
 
 #xgboost on magic data with out pca
 str(magicdata)
@@ -209,7 +276,10 @@ model_rf <- randomForest(train.data$class ~ . , train.data,ntree=1500,mtry=8)
 importance(model_rf)
 varImpPlot(model_rf)
 preds_rf <- predict(model_rf, test.data)
+confusionMatrix(preds_rf, test.data$class)$overall[1]
 confusionMatrix(preds_rf, test.data$class)
+
+preds_train_rf<-predict(model_rf)
 
 # model building on rf with out pca 
 model_rf <- randomForest(train.data.md$class ~ . , train.data.md,ntree=2000,mtry=8)
@@ -218,5 +288,118 @@ varImpPlot(model_rf)
 preds_rf <- predict(model_rf, test.data.md)
 confusionMatrix(preds_rf, test.data.md$class)
 
+#knn 
+library(class)
+library(DMwR)
+library(caret)
+model_knn <- knn3(class~.,train.data, k =5)
+preds <- predict(model_knn,test.data)
+#library(caret)
+preds_knn <- ifelse(preds[, 1] > preds[, 2], 0, 1)
+confusionMatrix(preds_knn,test.data$class)
 
 
+
+#knn wo pca
+model_knnwopca <- knn3(class~.,train.data.md, k =9,l = 0)
+
+predswopca<-predict(model_knnwopca,test.data.md)
+preds_knnwopca <- ifelse(predswopca[, 1] > predswopca[, 2], 0, 1)
+table(preds_knnwopca)
+confusionMatrix(preds_knnwopca,test.data.md$class)
+
+
+
+#knn
+trctrl <- trainControl(method = "repeatedcv", number = 10, repeats = 3)
+set.seed(3333)
+knn_fit <- train(class~., data = train.data, method = "knn", trControl=trctrl,preProcess = c("center", "scale"),tuneLength = 10)
+#We can see variation in Accuracy w.r.t K value by plotting these in a graph.
+plot(knn_fit)
+test_pred <- predict(knn_fit, newdata = test.data)
+table(test_pred)
+confusionMatrix(test_pred,test.data$class)
+
+#ANN
+train.x = data.matrix(train.data[,-8])
+train.y = as.numeric(as.character(train.data[,8]))
+str(train.y)
+test.x = data.matrix(test.data[,-8])
+test.y = as.numeric(as.character(test.data[,8]))
+library(mxnet)
+mx.set.seed(0)
+Sys.time() -> start
+model <- mx.mlp(train.x, train.y, hidden_node=c(10), out_node=2, activation="tanh", out_activation="softmax",
+                num.round=10, array.batch.size=100, learning.rate=0.07, momentum=0.9,
+                eval.metric=mx.metric.accuracy)
+Sys.time() -> end
+paste(end - start)
+preds = predict(model, test.x)
+preds=t(preds)
+pred.label = ifelse(preds[,2]<0.55, 0, 1)
+library(DMwR)
+library(caret)
+confusionMatrix(pred.label,test.y)
+
+ 
+#ann with out pca
+train.x = data.matrix(train.data.md[,-11])
+train.y = as.numeric(as.character(train.data.md[,11]))
+str(train.data.md)
+str(train.y)
+sum(is.na(train.data.md))
+test.x = data.matrix(test.data.md[,-11])
+test.y = as.numeric(as.character(test.data.md[,11]))
+library(mxnet)
+mx.set.seed(0)
+Sys.time() -> start
+model <- mx.mlp(train.x, train.y, hidden_node=c(10), out_node=2, activation="tanh", out_activation="softmax",
+                num.round=10, array.batch.size=100, learning.rate=0.07, momentum=0.9,
+                eval.metric=mx.metric.accuracy)
+Sys.time() -> end
+paste(end - start)
+preds = predict(model, test.x)
+preds=t(preds)
+pred.label = ifelse(preds[,2]<0.55, 0, 1)
+library(DMwR)
+library(caret)
+confusionMatrix(pred.label,test.y)
+
+
+
+
+
+# Example of Stacking algorithms
+# create submodels
+#install.packages("caretEnsemble")
+#library(caretEnsemble)
+#library(mlbench)
+#library(caret)
+#library(caretEnsemble)
+
+#control <- trainControl(method="repeatedcv", number=10, repeats=3, classProbs=TRUE)
+#algorithmList <- c('rpart', 'glm','knn')
+#set.seed(123)
+#metric <- "Accuracy"
+#models <- caretList(as.numeric(class)~., data=magicdata3, trControl=control,  methodList=algorithmList)
+#preds<-predict(models,magicdata,type='prob')
+#confusionMatrix(preds,magicdata$class)
+#results <- resamples(models)
+#summary(results)
+#dotplot(results)
+
+
+#staking 
+predDF <- data.frame(preds_rf, pred.dc_model,pred.ano,pred.tan,preds.c5, class = test.data$class)
+pred1V <- predict(model_rf, test.data)
+pred2V  <- predict(dc_fit, test.data)
+pred3v <- predict(model_svm.ano,test.data)
+pred4v<-predict(model.tanhdot,test.data)
+pred5v<-predict(c5_tree, test.data)
+predVD <- data.frame(pred.rf = pred1V, pred.dt = pred2V,pred.svm.ano=pred3v,pred.tanh=pred4v,pred.c5=pred5v)
+pred_test.svm <- predict(model_svm,test.data)
+accuracy <- rbind(confusionMatrix(pred1V, test.data$class)$overall[1],confusionMatrix(pred2V, test.data$class)$overall[1],confusionMatrix(test.data$class,pred_test.svm)$overall[1],confusionMatrix(pred.ano,test.data$class)$overall[1],confusionMatrix(pred.tan,test.data$class)$overall[1],confusionMatrix(preds.c5, test.data$class)$overall[1])
+row.names(accuracy) <- c("RF", "dt", "Stack","svmano","svmtan","c5")
+accuracy 
+dotplot(accuracy)
+ 
